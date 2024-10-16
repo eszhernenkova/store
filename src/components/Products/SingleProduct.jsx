5a0ecc8a2b8036e55c532539b4125a70ec708f3a
eszhernenkova <zhernenkova.ekaterina@gmail.com>
2024-10-16 22:40:26 +0400

    сделать кнопку favorites рабочей

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { useGetProductQuery } from '../../features/api/apiSlice';
+import { getRelatedProducts } from '../../features/products/productsSlice';
 
 import { ROUTES } from '../../utils/routes';
 
+import Product from './Product';
+import Products from './Products';
+
+
+
 const SingleProduct = () => {
+
+    const dispatch = useDispatch();
     const { id } = useParams();
     const navigate = useNavigate();
+    const {list, related } = useSelector(({ products }) => products)
 
     const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({ id });
 
@@ -16,9 +26,19 @@ const SingleProduct = () => {
             navigate(ROUTES.HOME)
         } 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isLoading, isFetching, isSuccess])
+    }, [isLoading, isFetching, isSuccess]);
+
+    useEffect(() => {
+      if(!data || !list.length) return;
+        dispatch(getRelatedProducts(data.category.id))
+    }, [data, dispatch, list.length]);
+
+
   return (
-    <div>SingleProduct</div>
+      <>
+        <Product {...data} />
+        <Products products = {related} amount={5} title="Related products" />
+      </>
   )
 }
 
